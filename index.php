2c70b10be5a3343067f989bb2135caec3ee2dc4a
Freakling42 <44568345+Freakling42@users.noreply.github.com>
2018-12-04 12:11:14 +0100

    Add files via upload
    add connection to database

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 
+require_once 'inc/classes/user.php';
 require_once('lib/smtemplate.php');
- 
+
+$user = new User($pdo);
+$list = $user->get_users();
+
+foreach($list as $test) {
+    echo $test["user_username"];
+}
+
 $tpl = new SMTemplate();
 $tpl->render('login');
